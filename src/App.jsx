719c4a06dfd9b03b092d58da778c97f78a42ebc6
gillesmendy <165325415+gillesmendy@users.noreply.github.com>
2024-10-16 19:35:29 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {RoutesPage} from "./router/Routes";
+ 
+function App() {
+
+  return (
+    <>
+      <RoutesPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+
+    return (
+      <>
+        <h1>Kasa</h1>
+        <h2>World</h2>
+      </>
+    )
+};
+  
+export default Home;
+  
